553f7d18890ab12afdacc6055a4f6fb49c628090
hjPark <hj.park@navercorp.com>
2024-04-03 20:56:38 +0900

    initialize project

---
+++ src/main/java/com/akfnt/cnsconfigservice/CnsConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akfnt.cnsconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akfnt/cnsconfigservice/CnsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akfnt.cnsconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
