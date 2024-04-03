1f9890b12a275098f4babbeaa12a793093e60766
hjPark <hj.park@navercorp.com>
2024-04-03 21:09:54 +0900

    setting up config server

--- src/main/java/com/akfnt/cnsconfigservice/CnsConfigServiceApplication.java
+++ src/main/java/com/akfnt/cnsconfigservice/CnsConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.akfnt.cnsconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CnsConfigServiceApplication {
 
 	public static void main(String[] args) {
